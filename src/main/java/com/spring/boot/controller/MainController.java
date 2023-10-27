d99c66a6b925264c973e42d1ca98d444e4d06124
charlievv92 <woocheolkim92@gmail.com>
2023-10-27 18:18:59 +0900

    베스트 상품 검색 기능 추가 완료 및 프로젝트의 properties 세팅 변경(쿼리문 사용 시 @Param을 인식하지 못하는 문제 해결 -> store information about method parameters 체크)

--- src/main/java/com/spring/boot/controller/MainController.java
+++ src/main/java/com/spring/boot/controller/MainController.java
@@ -1,8 +1,10 @@
 package com.spring.boot.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
@@ -21,6 +23,7 @@ import org.thymeleaf.spring5.SpringTemplateEngine;
 
 import com.spring.boot.dto.PageRequestDTO;
 import com.spring.boot.dto.PrincipalDetails;
+import com.spring.boot.service.OrderListService;
 import com.spring.boot.service.ProductService;
 
 import com.spring.boot.dto.PrincipalDetails;
@@ -36,6 +39,7 @@ public class MainController {
 
 	private final UserService userService;
 	private final ProductService productService;
+	private final OrderListService orderListService;
 	private final SpringTemplateEngine templateEngine;
 
 	
@@ -58,7 +62,7 @@ public class MainController {
 	
 	//메인에서 검색시이곳으로 keyword가 검색어
     @GetMapping("/search")
-    public String Search(@PageableDefault Pageable pageable, 
+    public String search( //@PageableDefault Pageable pageable 이미 pageable에 대한 설정 하고 왔음 
     		@RequestParam(value = "sort", required = false) String sort, 
 			@ModelAttribute PageRequestDTO pageRequestDTO, Model model, 
     		@AuthenticationPrincipal PrincipalDetails principalDetails) {
@@ -79,6 +83,8 @@ public class MainController {
     			sortText = "낮은 가격순";
     		}else if(sort.equals("priceDesc")) {
     			sortText = "높은 가격순";
+    		}else if(sort.equals("category")) {
+    			sortText = "카테고리순";
     		}
     		
             model.addAttribute("sort", sort);
@@ -93,6 +99,56 @@ public class MainController {
     	}
         
     	return "search";
+        
+    }
+    
+    //베스트 상품 페이징
+    @GetMapping("/searchBest")
+    public String searchBest( //@PageableDefault Pageable pageable, 
+    		@RequestParam(value = "sort", required = false) String sort, 
+			@ModelAttribute PageRequestDTO pageRequestDTO, Model model, 
+    		@AuthenticationPrincipal PrincipalDetails principalDetails) {
+        
+    	String sortText = "";
+    	
+    	if(principalDetails != null) {		
+    		SiteUser user = userService.getUserByUserName(principalDetails.getUsername());
+    		
+    		model.addAttribute("user", user);
+    	}
+    	
+    	//판매량 상위 20개
+    	Pageable top20 = PageRequest.of(0, 20);
+    	
+    	List<Long> top20SellingProductnos = orderListService.getTopNSellingProductnos(top20);
+    	List<Long> productnosTop20SellingNBoughtMoreThan3TimesBySameUser = orderListService
+    					.getProductnosBoughtMoreThan3TimesBySameUser(top20SellingProductnos);
+    	
+    	model.addAttribute("paging", productService.getBestProducts(productnosTop20SellingNBoughtMoreThan3TimesBySameUser, 
+    																pageRequestDTO, sort));
+    	if(sort != null && !sort.isEmpty()) {
+    		if(sort.equals("newest")) {
+    			sortText = "신상품순";
+    		}else if(sort.equals("priceAsc")) {
+    			sortText = "낮은 가격순";
+    		}else if(sort.equals("priceDesc")) {
+    			sortText = "높은 가격순";
+    		}else if(sort.equals("category")) {
+    			sortText = "카테고리순";
+    		}
+    		
+            model.addAttribute("sort", sort);
+            model.addAttribute("sortText", sortText);
+        }else {
+        	model.addAttribute("sort", "newest");
+        	model.addAttribute("sortText", "신상품순");
+        }
+    	
+//    	if(pageRequestDTO.getKeyword() != null && !pageRequestDTO.getKeyword().isEmpty()) {
+//    		model.addAttribute("keyword", pageRequestDTO.getKeyword());
+//    	}
+        
+    	return "searchBest";
         
     }
     
--- src/main/java/com/spring/boot/dao/OrderListRepository.java
+++ src/main/java/com/spring/boot/dao/OrderListRepository.java
@@ -2,6 +2,7 @@ package com.spring.boot.dao;
 
 import java.util.List;
 
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,7 +18,11 @@ public interface OrderListRepository extends JpaRepository<OrderList, Long>, Jpa
 	 List<OrderList> findByUser(SiteUser user);
 	 List<OrderList> findOrderByUser(SiteUser user);
 	 
-	 @Query("SELECT o.productno, SUM(o.count) FROM OrderList o GROUP BY o.productno ORDER BY SUM(o.count) DESC")
-	 Page<Object[]> findTopSellingProducts(Pageable pageable);
-	 
+	 //판매량 내림차순으로 productno 반환
+	 @Query("SELECT o.productno FROM OrderList o GROUP BY o.productno ORDER BY SUM(o.count) DESC")
+	 List<Long> findTopNSellingProductnos(Pageable pageable);
+
+	 //판매량 상위 20개의 productno 리스트로 재구매율이 높은(재구매 3회 이상) 상품의 productno와 구매한 userId 반환
+	 @Query("SELECT o.productno, o.user.id FROM OrderList o WHERE o.productno IN :productno GROUP BY o.productno, o.user.id HAVING COUNT(o) >= 3")
+	 List<Object[]> findProductsBoughtMoreThan3TimesBySameUser(@Param("productno") List<Long> productno);
 }
